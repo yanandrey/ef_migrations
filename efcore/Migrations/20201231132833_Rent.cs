9ae0201643becce22034b36fbfa9580132fccce7
Yan Andrey <yanandrey88@gmail.com>
2020-12-31 10:30:35 -0300

    rent migration

---
+++ efcore/Migrations/20201231132833_Rent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efcore.Migrations
+{
+    public partial class Rent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rent",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    VehicleId = table.Column<int>(type: "int", nullable: false),
+                    duration = table.Column<int>(type: "int", nullable: false),
+                    car_return = table.Column<DateTime>(type: "datetime", nullable: true),
+                    car_exit = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rent", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_rent_customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_rent_vehicle_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "vehicle",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_CustomerId",
+                table: "rent",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_VehicleId",
+                table: "rent",
+                column: "VehicleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rent");
+        }
+    }
+}
--- efcore/Models/Context/MyContext.cs
+++ efcore/Models/Context/MyContext.cs
@@ -10,12 +10,14 @@ namespace efcore.Models.Context
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Rent> Rents { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CustomerConfiguration());
             modelBuilder.ApplyConfiguration(new VehicleConfiguration());
             modelBuilder.ApplyConfiguration(new StaffConfiguration());
+            modelBuilder.ApplyConfiguration(new RentConfiguration());
         }
     }
 }
